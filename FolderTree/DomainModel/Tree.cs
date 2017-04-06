58eec8a60ae0e5058bc7e72a1d0cc275c12e6882
ILya-Lev <lis2889@gmail.com>
2017-04-06 20:41:07 +0300

    intial implementation

---
+++ FolderTree/DomainModel/Tree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DomainModel
+{
+	public class Tree
+	{
+		public FileSystemInfo Data { get; set; }
+		public Tree Root { get; set; }
+		public List<Tree> Clidren { get; } = new List<Tree>();
+
+		public bool IsEnumerated { get; set; } = false;
+
+		public static Tree TreeFrom(string path)
+		{
+			if (File.Exists(path))
+				return new Tree { Data = new FileInfo(path) };
+
+			if (Directory.Exists(path))
+				return new Tree { Data = new DirectoryInfo(path) };
+
+			throw new ArgumentException($"Path [{path}] is not either a file nor a directory - cannot traverse it");
+		}
+	}
+}
---
+++ FolderTree/FolderTraverser/Program.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLogic;
+using DomainModel;
+using System.Threading;
+
+namespace FolderTraverser
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var rootPath = @".\tree";
+			var outputFilePath = @".\output\tree.xml";
+
+			var root = Tree.TreeFrom(rootPath);
+
+			var traverser = new BusinessLogic.FolderTraverser(root);
+			var fileWriter = new FileWriter(outputFilePath, root);
+
+			var traverserThread = new Thread(traverser.Traverse);
+
+			var writerThread = new Thread(fileWriter.StoreTree);
+			writerThread.Start();
+
+			Thread.Sleep(1000);
+
+			traverserThread.Start();
+
+			traverserThread.Join();
+			writerThread.Join();
+		}
+	}
+}
